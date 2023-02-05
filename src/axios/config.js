031927642d39370ba8a4d0eae2ea3e1cf97e8ac1
dualsgo <contatombds@gmail.com>
2023-02-05 13:13:44 -0300

    get/ post

--- src/axios/config.js
+++ src/axios/config.js
@@ -2,7 +2,10 @@ import axios from "axios";
 
 const blogFetch = axios.create(
     {
-        baseURL:"https://jsonplaceholder.typicode.com"
+        baseURL:"https://jsonplaceholder.typicode.com",
+        headers:{
+            "Content-Type": "application/json",
+        }
     }
 )
 
--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import blogFetch from "../axios/config";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./NewPost.css"
 
 const NewPost = () => {
+
+  const navigate = useNavigate()
+
+  const [title, setTitle] = useState()
+  const [body, setBody] = useState()
+
+  const creatPost = async (e) => {
+    e.preventDefault();
+
+    const post = { title, body, userId: 1 };
+
+    await blogFetch.post("/posts", {
+      body: post
+    })
+navigate ("/")
+  }
   return (
-    <div>NewPost</div>
+    <div className='new-post'>
+      <h2>Inserir novo Post:</h2>
+      <form onSubmit={(e) => creatPost(e)}>
+        <div className='form-control'>
+          <label htmlFor="title">Título</label>
+          <input type="text" name='title' id='title' placeholder='Digite o título' onChange={(e) => setTitle(e.target.value)} />
+        </div>
+        <div className="form-control">
+          <label htmlFor="body">Conteúdo</label>
+          <input type="text" name='body' id='body' placeholder='Digite o conteúdo' onChange={(e) => setBody(e.target.value)} />
+        </div>
+        <input type="submit" value="Criar post" className='btn' />
+      </form>
+    </div>
   )
 }
 
