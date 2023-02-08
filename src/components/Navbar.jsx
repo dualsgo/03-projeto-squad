a2fea90490b2690185baf81fb657fe1244d3c1ed
dualsgo <contatombds@gmail.com>
2023-02-08 20:16:45 -0300

    save

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+/* import { Link } from "react-router-dom";
 
 import "./Navbar.css";
 
@@ -33,3 +33,39 @@ const Navbar = () => {
 };
 
 export default Navbar;
+ */
+
+import { Link } from "react-router-dom";
+
+import "./Navbar.css";
+
+const Navbar = () => {
+
+  return (
+
+      <nav class="menu">
+        <ul>
+          <li>Painel administrativo</li>
+          <li>
+            <a href="#">Cadastros</a>
+            <ul>
+              <li><Link to={`/`}>Lista: Sedans</Link></li>
+              <li><Link to={`/hatch`}>Lista: Hatchs</Link></li>
+              <li><Link to={`/suv`} >Lista: SUVs</Link></li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">Editor</a>
+            <ul>
+              <li><Link to={`/admin`}>Gerenciar: Sedans</Link></li>
+              <li><Link to={`/admin2`}>Gerenciar: Hatchs</Link></li>
+              <li><Link to={`/suv`} >Lista: SUVs</Link></li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+      );
+};
+
+
+      export default Navbar;
