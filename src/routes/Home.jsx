3a7c7704c29c2547e8c46eb40402f7ea05731975
dualsgo <73504705+dualsgo@users.noreply.github.com>
2023-02-15 19:40:11 -0300

    Adicionando cards

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -20,28 +20,40 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="home">
-      <div className="containerdiv">
-        <Link className="" to={`/Admin`}>
-          <div className="">
-            <h2>Andar 1</h2>
-          </div>
-        </Link>
-        <br />
-        <Link className="" to={`/Admin2`}>
-          <div className="">
-            <h2>Andar 2</h2>
-          </div>
-        </Link>
-        <br />
-        <Link className="" to={`/Admin3`}>
-          <div className="">
-            <h2>Andar 3</h2>
-          </div>
-        </Link>
+<div class="cardsHome">
+  <div class="card">
+    <div class="box">
+      <div class="content">
+        <h2>01</h2>
+        <h3>Primeiro andar</h3>
+        <p>Veículos estacionados nas vagas do primeiro andar</p>
+        <Link className="btn-btn" to={`/Andar1`}>Visualizar</Link>
       </div>
-      
     </div>
+  </div>
+
+  <div class="card">
+    <div class="box">
+      <div class="content">
+        <h2>02</h2>
+        <h3>Segundo andar</h3>
+        <p>Veículos estacionados nas vagas do segundo andar</p>
+        <Link className="btn-btn" to={`/Andar2`}>Visualizar</Link>
+      </div>
+    </div>
+  </div>
+
+  <div class="card">
+    <div class="box">
+      <div class="content">
+        <h2>03</h2>
+        <h3>Terceiro andar</h3>
+        <p>Veículos estacionados nas vagas do terceiro andar</p>
+        <Link className="btn-btn" to={`/Andar3`}>Visualizar</Link>
+      </div>
+    </div>
+  </div>
+</div>
   );
 };
 
